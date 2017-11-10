96ff91c23f702279b585318cfa765a61f6c053ce
firewithwind <liulianxing@liulianxingdeMacBook-Pro.local>
2017-11-10 18:57:12 +0800

    vue小记

--- README.md
+++ README.md
@@ -44,6 +44,67 @@ Vue学习小记
 
 *但是有些时候子组件会想要改变接收到的值，一般会用$emit来向父组件发送通知，要求调用函数。vue中提供了.sync修饰符，实际是一个语法糖，调用后执行prop数据传输和父组件函数定义，子组件一旦改变带有.sync修饰符的参数，就会调用$emit向父组件通知*
 
+###Vue过渡与动画
+
+vue为元素添加过渡或者动画用transition标签
+
+1、初次渲染过渡可以用appear特性
+
+```
+<transition appear>
+	...
+</transition>
+```
+2、多个元素过渡，v-if切换拥有相同标签名的标签时，应该为元素设置key属性，否则不会产生切换效果
+
+```
+<transition>
+  <button v-if="isEditing" key="save">
+    Save
+  </button>
+  <button v-else key="edit">
+    Edit
+  </button>
+</transition>
+```
+3、使用多个v-if的多个过渡元素可以重写为一个绑定了动态属性的过渡元素
+
+```
+<transition>
+  <button v-if="docState === 'saved'" key="saved">
+    Edit
+  </button>
+  <button v-if="docState === 'edited'" key="edited">
+    Save
+  </button>
+  <button v-if="docState === 'editing'" key="editing">
+    Cancel
+  </button>
+</transition>
+```
+重写=>
+
+```
+<transition>
+	<button :key="docState">{{ buttonMessage }}</button>
+</transition>
+```
+
+```
+computed: {
+	buttonMessage: function() {
+		switch(this.docState) {
+			case 'saved': return 'Edit';
+	      	case 'edited': return 'Save';
+	      	case 'editing': return 'Cancel';
+		}
+	}
+}
+```
+4、列表过渡一般用transition-group,该标签在渲染时会产生一个真实的span元素,可以用tag属性来更改产生的标签类型。
+
+
+
 
 
 
--- src/pages/index/mytemplate.vue
+++ src/pages/index/mytemplate.vue
@@ -1,20 +1,59 @@
 <template>
-	<div class="mytemplate">
-		<h1>this is a title</h1>
-	</div>	
+    <div class="mytemplate">
+       <h1>this is a title</h1>
+        <div class="test-transition">
+            <transition name="fade">
+                <span class="test1" v-if="test1">show or hide</span>
+            </transition>
+            <div>
+                <button @click="test1=true">show</button>
+                <button @click="test1=false">hide</button>
+            </div>
+            <div class="test2">
+                <div>
+                    <button v-on:click="add">Add</button>
+                    <button v-on:click="remove">Remove</button>
+                </div>
+                <transition-group name="list" tag="p">
+                    <span v-for="item in items" v-bind:key="item" class="list-item">
+                      {{ item }}
+                    </span>
+              </transition-group>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-	data() {
-		return {
-			attr: ''
-		}
-	},
-	created() {
-		console.log(this.$ref.tit)
-	}
+    data() {
+        return {
+            attr: '',
+            test1: true,
+            items: [1, 2, 3, 4, 5, 6],
+            nextNum: 10
+       }
+    },
+    created() {
+       console.log(this.$refs.tit)
+    },
+    methods: {
+        randomIndex: function () {
+          return Math.floor(Math.random() * this.items.length)
+        },
+        add: function () {
+          this.items.splice(this.randomIndex(), 0, this.nextNum++)
+        },
+        remove: function () {
+          this.items.splice(this.randomIndex(), 1)
+        },
+    }
 }
 </script>
 <style scoped rel="stylesheet/scss" lang="scss">
-	
+.fade-enter-active, .fade-leave-active {
+    transition: all .5s;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
 </style>
